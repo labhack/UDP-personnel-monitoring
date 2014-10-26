bd47546027aa3c9a53c85f4cc00c93978d2035ff
Val A. Red <vred@rutgers.edu>
2014-10-26 13:25:50 -0400

    Final push

--- server_script.py
+++ server_script.py
@@ -1,23 +1,41 @@
-import os
+#
+# Labhack: UDP Personal Monitoring 
+# 10/26/2014
+#
+# GNU GPL License
+#
+
+from os import listdir
 import os.path
+import subprocess
 import time
 import struct
+import shutil
 
-filenum = 1
-pathSource = '/home/devinws/Data/'
-pathDestination = '/home/devinws/Processed/'
+#data sources - adapt to server
+pathSource = "/home/devinws/Data/"
+pathDestination = "/home/devinws/Processed/"
+pathArchive = "/home/devinws/Archive/"
 
-epoch = []
-lat = []
-lon = []
-alt = []
-ecg = []
-o2 = []
-resp = []
-temp = []
+#arrays for data processing
+#inefficient
+epoch_last = 0
+lat_last = 0
+lon_last = 0
+alt_last = 0
+ecg_last = 0
+o2_last = 0
+resp_last = 0
+temp_last = 0
+ip_last = 0
 
-while True:
-	fname = pathSource + str(filenum) + ".ebola"
+#run server-side continously
+while True :
+	if os.listdir(pathSource) == []:
+		continue
+	files = listdir(pathSource)
+	# We used this for debugging: # print (files)
+	fname = pathSource + str(files[0]) 
 	
 	deltaLatitude = 0
 	deltaLongitude = 0
@@ -32,67 +50,83 @@ while True:
 		fih.close()
 		
 		data[7] = data[7].strip('\n')
+		data[7] = data[7].strip('\r')
+
+		epoch = data[0]
+		lat = data[1]
+		lon = data[2]
+		alt = data[3]
+		ecg = data[4]
+		o2 = data[5]
+		resp = data[6]
+		temp = data[7]
+		ip = data[8]
 		
-		epoch.append(data[0])
-		lat.append(data[1])
-		lon.append(data[2])
-		alt.append(data[3])
-		ecg.append(data[4])
-		o2.append(data[5])
-		resp.append(data[6])
-		temp.append(data[7])
+		deltaLatitude = float(lat)-float(lat_last)
+		deltaLongitude = float(lon)-float(lon_last)
+		deltaAltitude = float(alt)-float(alt_last)
 		
-		deltaLatitude = float(lat[filenum-1])-float(lat[filenum-2])
-		deltaLongitude = float(lon[filenum-1])-float(lon[filenum-2])
-		deltaAltitude = float(alt[filenum-1])-float(alt[filenum-2])
-		
-		if int(ecg[filenum-1]) > 180:
+		rgbEcg = 'FFFFFF'
+		if int(ecg) > 180:
 			rgbEcg = 'FF0000'
-		elif int(ecg[filenum-1]) >= 160:
+		elif int(ecg) >= 160:
 			rgbEcg = 'FF8000'
-		elif int(ecg[filenum-1]) > 140:
+		elif int(ecg) > 140:
 			rgbEcg = 'FFC800'
-		elif int(ecg[filenum-1]) >= 120:
+		elif int(ecg) >= 120:
 			rgbEcg = 'FFFF00'
-		elif int(ecg[filenum-1]) > 40:
+		elif int(ecg) > 40:
 			rgbEcg = '00FF00'
-		elif int(ecg[filenum-1]) <= 40:
+		elif int(ecg) <= 40:
 			regEcg = 'FF0000'
 			
-		if int(o2[filenum-1]) > 95:
+		if int(o2) > 95:
 			rgbo2 = '00FF00'
 		else:
 			rgbo2 = 'FF0000'
 			
-		if int(resp[filenum-1]) > 30:
+		if int(resp) > 30:
 			rgbResp = 'FF0000'
-		elif int(resp[filenum-1]) > 8:
+		elif int(resp) > 8:
 			rgbResp = '00FF00'	
 		else:
 			rgbResp = 'FF0000'	
 			
-		if float(temp[filenum-1]) > 100:
+		if float(temp) > 100:
 			rgbTemp = 'FF0000'
-		elif float(temp[filenum-1]) > 96:
+		elif float(temp) > 96:
 			rgbTemp = '00FF00'	
 		else:
 			rgbTemp = 'FF0000'	
 			
-		out = 'time:{0}'.format(epoch[filenum-1]) + ':FFFFFF\n'
-		out += 'latitude:{0}'.format(lat[filenum-1]) + ':FFFFFF\n'
+		out = 'time:{0}'.format(epoch) + ':FFFFFF\n'
+		out += 'latitude:{0}'.format(lat) + ':FFFFFF\n'
 		out += 'deltaLatitude:{0}'.format(deltaLatitude) + ':FFFFFF\n'
-		out += 'longitude:{0}'.format(lon[filenum-1]) + ':FFFFFF\n'
+		out += 'longitude:{0}'.format(lon) + ':FFFFFF\n'
 		out += 'deltaLongitude:{0}'.format(deltaLongitude) + ':FFFFFF\n'
-		out += 'altitude:{0}'.format(alt[filenum-1]) + ':FFFFFF\n'
+		out += 'altitude:{0}'.format(alt) + ':FFFFFF\n'
 		out += 'deltaAltitude:{0}'.format(deltaAltitude) + ':FFFFFF\n'
-		out += 'ECG:{0}'.format(ecg[filenum-1]) + ':{0}\n'.format(rgbEcg)
-		out += 'O2:{0}'.format(o2[filenum-1]) + ':{0}\n'.format(rgbo2)		
-		out += 'resp:{0}'.format(resp[filenum-1]) + ':{0}\n'.format(rgbResp)				
-		out += 'temp:{0}'.format(temp[filenum-1]) + ':{0}\n'.format(rgbTemp)
+		out += 'ECG:{0}'.format(ecg) + ':{0}\n'.format(rgbEcg)
+		out += 'O2:{0}'.format(o2) + ':{0}\n'.format(rgbo2)		
+		out += 'resp:{0}'.format(resp) + ':{0}\n'.format(rgbResp)				
+		out += 'temp:{0}'.format(temp) + ':{0}\n'.format(rgbTemp)
+		out += 'IP:{0}'.format(ip) + ':FFFFFF\n'
 
-		foh = open(pathDestination + str(filenum) + ".ebola", 'w+')
+		foh = open(pathDestination + files[0], 'w+')
 		foh.write(out)
 		foh.close()
 		
+		if os.listdir(pathSource) != []:
+			subprocess.call(["rm", pathSource + files[0]])
+
+		epoch_last = 0
+		lat_last = 0
+		lon_last = 0
+		alt_last = 0
+		ecg_last = 0
+		o2_last = 0
+		resp_last = 0
+		temp_last = 0
+		ip_last = 0
+
 		print(out)
-		filenum = filenum + 1
